e78bdb59b56051c01f5990075355e5d518528d95
kpotard <kevin.potard@capgemini.com>
2019-02-28 13:57:11 +0100

    add buffer and osm search

--- inst/app/app.R
+++ inst/app/app.R
@@ -5,13 +5,15 @@ library(ggmap)
 library(shinydashboard)
 library(DT)
 library(sf)
+library(tmap)
+library(leaflet.extras)
 
 this_table = data.frame(lat = NULL, lng = NULL, Distance = NULL)
 
 ui <- fluidPage(
-  navbarPage("Où Vivre", id="nav",
+  tabsetPanel(id="nav",
 
-             tabPanel("Interactive map",
+             tabPanel("InteractiveMap",
                       tags$head(
                         # Include our custom CSS
                         includeCSS("./www/style.css")
@@ -37,9 +39,11 @@ ui <- fluidPage(
                                'All Data are available on ', tags$em('https://www.data.gouv.fr/fr/'), ' and compiled by Kevin POTARD.'
                       )
              ),
-             tabPanel("Data"
+             tabPanel("result",
+                      leafletOutput("my_tmap")
 
-                      )
+                      ),
+             tabPanel("Data")
   )
 )
 
@@ -48,6 +52,7 @@ server <- function(input, output, session) {
   output$map<- renderLeaflet({
     leaflet(options = leafletOptions(minZoom = 6, dragging = T))%>%
       addProviderTiles(provider = "OpenStreetMap.France")%>%
+      addSearchOSM()%>%
       setView(lng = 2.43, lat=46.53,zoom = 7) %>%
       setMaxBounds(lng1 = 2.43 + 9,
                    lat1 = 46.53 + 12,
@@ -101,16 +106,29 @@ server <- function(input, output, session) {
   })
 
   observeEvent(input$generateZone,{
-    df_sf <- st_as_sf(this_table(), coords = c("lng", "lat"), crs=4326)
-     print(df_sf)
 
-     df_buf <- st_buffer(df_sf, dist = df_sf$Distance)
-     st_geometry(df_buf)
-     leafletProxy('map') %>% # use the proxy to save computation
-       addCircles(st_geometry(df_buf))
+
+     updateTabsetPanel(session, "nav",
+                       selected = "result")
   })
 
+  # ----------------- Result Panel
 
+  output$my_tmap = renderLeaflet({
+    df_sf <- st_as_sf(this_table(), coords = c("lng", "lat"), crs=4326)
+    df_sf<- st_transform(df_sf, crs=3857)
+
+    df_buf <- st_buffer(df_sf, dist = df_sf$Distance)
+    geom<-st_geometry(df_buf)
+    geom_union<-st_union(geom)
+
+    geom_union_tran <- st_transform(geom_union,  crs=4326)
+
+    proxy <- leafletProxy('map')
+    proxy%>%
+      clearMarkers()%>%
+      addPolygons(data=geom_union_tran)
+  })
 }
 
 shinyApp(ui, server)
